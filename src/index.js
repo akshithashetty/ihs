fd5d82495a81520c684d1a78a53e942f8814ab42
Kaiz <kaiz.growfitter@gmail.com>
2018-12-03 18:15:05 +0530

    redux-persist works

--- src/index.js
+++ src/index.js
@@ -8,11 +8,25 @@ import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import storage from 'redux-persist/lib/storage';
 import Cart from './_Redux/reducers/Cart/Cart'
 
-const store = createStore(Cart);
+// const store = createStore(Cart);
 
-ReactDOM.render((<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>), document.getElementById('root'));
+const persistConfig = {
+ key: 'root',
+ storage: storage,
+ stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
+};
+
+const pReducer = persistReducer(persistConfig, Cart);
+export const store = createStore(pReducer);
+export const persistor = persistStore(store);
+
+ReactDOM.render((<Provider store={store}><PersistGate persistor={persistor}><BrowserRouter><App /></BrowserRouter></PersistGate></Provider>), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
